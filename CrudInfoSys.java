001333d162e92ebc1e0c32b6cc461d097bd00fc5
James <mocryan@yahoo.com>
2022-03-23 23:08:23 +0800

    first commit

---
+++ CrudInfoSys.java
@@ -0,0 +1,233 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+import javax.swing.DefaultComboBoxModel;
+
+public class CrudInfoSys {
+
+	private JFrame frame;
+	private JTextField textFirstName;
+	private JTextField textMiddleName;
+	private JTextField textFamilyName;
+	private JTextField textDoB;
+	private JTextField textSSN;
+	private JTextField textCurrentAddress;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CrudInfoSys window = new CrudInfoSys();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CrudInfoSys() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(240, 240, 240));
+		frame.setBounds(100, 100, 592, 539);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JPanel panel = new JPanel();
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setBounds(10, 11, 556, 254);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("First Name\r\n");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel.setBounds(10, 9, 68, 20);
+		panel.add(lblNewLabel);
+
+		JLabel lblNewLabel_1 = new JLabel("Middle Name");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_1.setBounds(10, 36, 77, 20);
+		panel.add(lblNewLabel_1);
+
+		JLabel lblNewLabel_2 = new JLabel("Family Name");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2.setBounds(10, 59, 77, 20);
+		panel.add(lblNewLabel_2);
+
+		textFirstName = new JTextField();
+		textFirstName.setBounds(90, 9, 146, 20);
+		panel.add(textFirstName);
+		textFirstName.setColumns(10);
+
+		textMiddleName = new JTextField();
+		textMiddleName.setColumns(10);
+		textMiddleName.setBounds(90, 36, 146, 20);
+		panel.add(textMiddleName);
+
+		textFamilyName = new JTextField();
+		textFamilyName.setColumns(10);
+		textFamilyName.setBounds(90, 59, 146, 20);
+		panel.add(textFamilyName);
+
+		JLabel lblNewLabel_3 = new JLabel("Bio. Gender");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_3.setBounds(246, 12, 86, 14);
+		panel.add(lblNewLabel_3);
+
+		JLabel lblNewLabel_4 = new JLabel("Date of Birth");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_4.setBounds(246, 37, 86, 20);
+		panel.add(lblNewLabel_4);
+
+		JLabel lblNewLabel_2_1 = new JLabel("Suffix\r\n");
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2_1.setBounds(10, 89, 68, 20);
+		panel.add(lblNewLabel_2_1);
+
+		JComboBox boxSuffix = new JComboBox();
+		boxSuffix.setModel(new DefaultComboBoxModel(new String[] { "None.", "Jr.", "Sr." }));
+		boxSuffix.setBounds(90, 89, 146, 22);
+		panel.add(boxSuffix);
+
+		JComboBox boxGender = new JComboBox();
+		boxGender.setModel(new DefaultComboBoxModel(new String[] { "None.", "MALE", "FEMALE" }));
+		boxGender.setBounds(342, 9, 185, 22);
+		panel.add(boxGender);
+
+		textDoB = new JTextField();
+		textDoB.setBounds(342, 37, 185, 20);
+		panel.add(textDoB);
+		textDoB.setColumns(10);
+
+		JLabel lblNewLabel_4_1 = new JLabel("SSN");
+		lblNewLabel_4_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_4_1.setBounds(246, 59, 86, 20);
+		panel.add(lblNewLabel_4_1);
+
+		JLabel lblNewLabel_4_2 = new JLabel("Nationality");
+		lblNewLabel_4_2.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_4_2.setBounds(246, 89, 86, 20);
+		panel.add(lblNewLabel_4_2);
+
+		textSSN = new JTextField();
+		textSSN.setColumns(10);
+		textSSN.setBounds(342, 60, 185, 20);
+		panel.add(textSSN);
+
+		JLabel lblNewLabel_2_1_1 = new JLabel("Current Address");
+		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_2_1_1.setBounds(10, 120, 226, 20);
+		panel.add(lblNewLabel_2_1_1);
+
+		textCurrentAddress = new JTextField();
+		textCurrentAddress.setBounds(116, 121, 430, 88);
+		panel.add(textCurrentAddress);
+		textCurrentAddress.setColumns(10);
+
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textFirstName.setText(null);
+				textMiddleName.setText(null);
+				textFamilyName.setText(null);
+				textDoB.setText(null);
+				textSSN.setText(null);
+				textCurrentAddress.setText(null);
+			}
+		});
+		btnClear.setBounds(457, 220, 89, 23);
+		panel.add(btnClear);
+		
+		JComboBox boxNatl = new JComboBox();
+		boxNatl.setModel(new DefaultComboBoxModel(new String[] {"Afghan", "Albanian", "Algerian", "American", "Andorran", "Angolan", "Antiguans", "Argentinean", "Armenian", "Australian", "Austrian", "Azerbaijani", "Bahamian", "Bahraini", "Bangladeshi", "Barbadian", "Barbudans", "Batswana", "Belarusian", "Belgian", "Belizean", "Beninese", "Bhutanese", "Bolivian", "Bosnian", "Brazilian", "British", "Bruneian", "Bulgarian", "Burkinabe", "Burmese", "Burundian", "Cambodian", "Cameroonian", "Canadian", "Cape Verdean", "Central African", "Chadian", "Chilean", "Chinese", "Colombian", "Comoran", "Congolese", "Costa Rican", "Croatian", "Cuban", "Cypriot", "Czech", "Danish", "Djibouti", "Dominican", "Dutch", "East Timorese", "Ecuadorean", "Egyptian", "Emirian", "Equatorial Guinean", "Eritrean", "Estonian", "Ethiopian", "Fijian", "Filipino", "Finnish", "French", "Gabonese", "Gambian", "Georgian", "German", "Ghanaian", "Greek", "Grenadian", "Guatemalan", "Guinea-Bissauan", "Guinean", "Guyanese", "Haitian", "Herzegovinian", "Honduran", "Hungarian", "I-Kiribati", "Icelander", "Indian", "Indonesian", "Iranian", "Iraqi", "Irish", "Israeli", "Italian", "Ivorian", "Jamaican", "Japanese", "Jordanian", "Kazakhstani", "Kenyan", "Kittian and Nevisian", "Kuwaiti", "Kyrgyz", "Laotian", "Latvian", "Lebanese", "Liberian", "Libyan", "Liechtensteiner", "Lithuanian", "Luxembourger", "Macedonian", "Malagasy", "Malawian", "Malaysian", "Maldivian", "Malian", "Maltese", "Marshallese", "Mauritanian", "Mauritian", "Mexican", "Micronesian", "Moldovan", "Monacan", "Mongolian", "Moroccan", "Mosotho", "Motswana", "Mozambican", "Namibian", "Nauruan", "Nepalese", "New Zealander", "Ni-Vanuatu", "Nicaraguan", "Nigerian", "Nigerien", "North Korean", "Northern Irish", "Norwegian", "Omani", "Pakistani", "Palauan", "Panamanian", "Papua New Guinean", "Paraguayan", "Peruvian", "Polish", "Portuguese", "Qatari", "Romanian", "Russian", "Rwandan", "Saint Lucian", "Salvadoran", "Samoan", "San Marinese", "Sao Tomean", "Saudi", "Scottish", "Senegalese", "Serbian", "Seychellois", "Sierra Leonean", "Singaporean", "Slovakian", "Slovenian", "Solomon Islander", "Somali", "South African", "South Korean", "Spanish", "Sri Lankan", "Sudanese", "Surinamer", "Swazi", "Swedish", "Swiss", "Syrian", "Taiwanese", "Tajik", "Tanzanian", "Thai", "Togolese", "Tongan", "Trinidadian or Tobagonian", "Tunisian", "Turkish", "Tuvaluan", "Ugandan", "Ukrainian", "Uruguayan", "Uzbekistani", "Venezuelan", "Vietnamese", "Welsh", "Yemenite", "Zambian", "Zimbabwean"}));
+		boxNatl.setMaximumRowCount(194);
+		boxNatl.setBounds(342, 89, 185, 22);
+		panel.add(boxNatl);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_1.setBounds(10, 276, 556, 44);
+		frame.getContentPane().add(panel_1);
+		panel_1.setLayout(null);
+
+		JButton btnView = new JButton("View");
+		btnView.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnView.setBounds(10, 11, 125, 23);
+		panel_1.add(btnView);
+
+		JButton btnCreate = new JButton("Create");
+		btnCreate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String fName, mName, fmName, sfx, gender, dateOfBirth, SocialSecurityNumber, Nationality,
+						CurrentAddress;
+
+				fName = textFirstName.getText();
+				mName = textMiddleName.getText();
+				fmName = textFamilyName.getText();
+				dateOfBirth = textDoB.getText();
+				SocialSecurityNumber = textSSN.getText();
+				CurrentAddress = textCurrentAddress.getText();
+				Nationality = (String) boxNatl.getSelectedItem();
+				gender = (String) boxGender.getSelectedItem();
+				sfx = (String) boxSuffix.getSelectedItem();
+			}
+		});
+		btnCreate.setBounds(145, 11, 127, 23);
+		panel_1.add(btnCreate);
+
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setBounds(282, 11, 127, 23);
+		panel_1.add(btnUpdate);
+
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnDelete.setBounds(419, 11, 127, 23);
+		panel_1.add(btnDelete);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel_2.setBounds(10, 331, 556, 158);
+		frame.getContentPane().add(panel_2);
+		panel_2.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 536, 136);
+		panel_2.add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+	}
+}
